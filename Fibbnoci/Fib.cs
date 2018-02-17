2c0927c46afa07ba57d514c1502470a7cacd4562
nikhil muthyala <s528754@nwmissouri.edu>
2018-02-16 20:42:38 -0600

    uploading to github

---
+++ Fibbnoci/Fib.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibbnoci
+{
+    class Fib
+    {
+        public void logic()
+        {
+            try
+            {
+                int n, a = 0, b = 1, temp;
+                Console.WriteLine("enter value of n:");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(1);
+                for (int i = 1; i < n; i++)
+                {
+                    temp = a + b;
+                    a = b;
+                    b = temp;
+                    Console.WriteLine(temp);
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.ReadLine();
+            }
+          
+
+        }
+    }
+}
---
+++ Fibbnoci/MainFibonocci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibbnoci
+{
+    
+    class MainFibonocci
+    {
+        static void Main(string[] args)
+        {
+            Fib obj = new Fib();
+            obj.logic();
+        }
+
+    }
+}
+
+
